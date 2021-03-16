f6148e8df4689267c2703ef868d3713f4b25bdf7
Jeremy Costanza <costanza.jeremy@gmail.com>
2021-03-15 20:53:03 -0400

    Add Jenkinsfile

--- build.sh
+++ build.sh
@@ -18,8 +18,11 @@ echo " ########## AWS Glue for hive metastore .... "
 cd ..
 mvn clean package -DskipTests -pl -aws-glue-datacatalog-hive2-client
 
+rm -rf dist/jars
 mkdir -p dist/jars
 find . -path ./hive -prune -false -o -name "*.jar" | grep -Ev "test|original" | xargs -I{} cp {} ./dist/jars
 
-echo " ########## Success !!! "
+echo " ########## Copy to S3 "
+aws s3 cp --recursive dist/jars/ s3://kf-strides-variant-parquet-prd/ami_libraries/aws-glue-metastore-client-spark3/
+
 
